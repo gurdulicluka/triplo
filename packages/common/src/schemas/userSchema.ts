892b2fae2657ea8c4fee4627105fb148ec715bd0
Luka Gurdulic <gurdulicluka@gmail.com>
2024-10-01 11:52:54 +0200

    fixes

--- packages/common/src/schemas/userSchema.ts
+++ packages/common/src/schemas/userSchema.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  email: z.string().email(),
+	id: z.string(),
+	name: z.string(),
+	email: z.string().email(),
 });
 
 export type User = z.infer<typeof userSchema>;
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,4 @@
-import { User } from "@triplo/common";
+import type { User } from "@triplo/common";
 
 const user: User = { email: "worenw", id: "123", name: "q4" };
 console.log(user);
