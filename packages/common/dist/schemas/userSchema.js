c0716aafbb7b166d8625fa46637786b3e2d8d7fa
Luka Gurdulic <gurdulicluka@gmail.com>
2024-10-01 12:10:35 +0200

    add common build script

--- packages/common/dist/schemas/userSchema.js
+++ packages/common/dist/schemas/userSchema.js
@@ -1,6 +1,6 @@
 import { z } from "zod";
 export const userSchema = z.object({
-    id: z.string(),
-    name: z.string(),
-    email: z.string().email(),
+	id: z.string(),
+	name: z.string(),
+	email: z.string().email(),
 });
